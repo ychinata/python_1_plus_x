0befe10c6549030825b3d7d1a77214f9355d8456
ychinata <ychinata@qq.com>
2022-04-02 00:00:36 +0800

    No.: Pygame_Alien_Invasion Author:ychinata Description:蟒蛇书《Python编程-从入门到实践》13章内容：Pygame游戏. 创建一行外星人

--- python_just_for_fun/2.game/1.alien_invasion/alien_invasion.py
+++ python_just_for_fun/2.game/1.alien_invasion/alien_invasion.py
@@ -8,11 +8,12 @@ import sys
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
+"""管理游戏资源和行为的类"""
 class AlienInvasion:
-    """管理游戏资源和行为的类"""
+    """初始化游戏并创建游戏资源"""
     def __init__(self):
-        """初始化游戏并创建游戏资源"""
         pygame.init()
         self.settings = Settings()
         # 游戏不全屏，指定窗口大小
@@ -27,18 +28,21 @@ class AlienInvasion:
         self.ship = Ship(self)
         # 子弹
         self.bullets = pygame.sprite.Group()
+        # 外星人
+        self.aliens = pygame.sprite.Group()
+        self._creat_fleet()
         # 设置背景色
         self.bg_color = (230, 230, 230)
 
+    """开始游戏主循环"""
     def run_game(self):
-        """开始游戏主循环"""
         while True:
-            """监视键盘和鼠标事件"""
+            # 监视键盘和鼠标事件
             self._check_events_()
             self.ship.update()
             self._update_bullets()
 
-            """每次循环时都重绘屏幕"""
+            # 每次循环时都重绘屏幕
             self._update_screen()
 
     # 辅助方法命名以_开头
@@ -54,8 +58,8 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
+    """响应按键按下"""
     def _check_keydown_events(self, event):
-        """响应按键按下"""
         # 系统时如何同时检测到左右键同时按下的？
         # 按下左右键移动飞船
         if event.key == pygame.K_RIGHT:
@@ -69,22 +73,22 @@ class AlienInvasion:
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
 
+    """响应按键松开"""
     def _check_keyup_events(self, event):
-        """响应按键松开"""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    """创建一颗子弹，并将其加入编组bullets中"""
     def _fire_bullet(self):
-        """创建一颗子弹，并将其加入编组bullets中"""
         # 限制子弹数量
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
 
+    """更新子弹的位置并删除消失的子弹"""
     def _update_bullets(self):
-        """更新子弹的位置并删除消失的子弹"""
         self.bullets.update()
         # 删除消失的子弹
         for bullet in self.bullets.copy():
@@ -92,12 +96,33 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
         # print(len(self.bullets))
 
+    """创建外星人群"""
+    def _creat_fleet(self):
+        # 创建一个外星人
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+        # 创建第一行外星人
+        for alien_number in range(number_aliens_x):
+            self._creat_alien(alien_number)
+
+    """创建一个外星人并将其放在当前行"""
+    def _creat_alien(self, alien_number):
+        # 创建一个外星人并将其加入当前行
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
+
     """每次循环时都重绘屏幕"""
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
